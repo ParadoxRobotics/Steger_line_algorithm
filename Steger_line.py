e6dfb78f4b8b462d01d4a5b0654cec1394f7a2fb
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-01-24 16:41:26 +0100

    Create Steger_line.py
    first commit

---
+++ Steger_line.py
@@ -0,0 +1,67 @@
+# Model-based learning of object for tracking and pose estimation
+# Author : Munch Quentin, 2020
+
+# General and computer vision lib
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib import pyplot
+
+# convert to grayscle
+img = cv2.imread("desk.png")
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# calculate hessian matrix (dx, dy, dxy) fot the current image
+# first order derivative using sobel
+dx = cv2.Sobel(gray_img, cv2.CV_32F, 2, 0, ksize=3)
+dy = cv2.Sobel(gray_img, cv2.CV_32F, 0, 2, ksize=3)
+dxy = cv2.Sobel(gray_img, cv2.CV_32F, 1, 1, ksize=3)
+
+# create gaussian kernel
+gauss_size = 11
+gauss_kernel = cv2.getGaussianKernel(gauss_size, -1, cv2.CV_32F)
+gauss_kernel_T = np.transpose(gauss_kernel)
+# second order derivative with applied gaussian filter
+ddx = cv2.sepFilter2D(dx,-1, gauss_kernel_T, gauss_kernel)
+ddy = cv2.sepFilter2D(dy,-1, gauss_kernel_T, gauss_kernel)
+ddxy = cv2.sepFilter2D(dxy,-1, gauss_kernel_T, gauss_kernel)
+# extract edge
+edge = np.sqrt(dx**2 + dy**2)
+
+# calculate eigen vector
+tmp = np.sqrt((ddx - ddy)**2 + 4*ddxy**2)
+v2x = 2*ddxy
+v2y = ddy - ddx + tmp
+# normalization
+magnitude = cv2.magnitude(ddx, ddy)
+i = (magnitude != 0);
+v2x[i] = v2x[i]/magnitude[i];
+v2y[i] = v2y[i]/magnitude[i];
+# The eigenvectors are orthogonal
+v1x = -v2y
+v1y = v2x
+# Compute the eigenvalues
+mu1 = 0.5*(ddx + ddy + tmp)
+mu2 = 0.5*(ddx + ddy - tmp)
+# Sort eigen values by absolute value abs(Lambda1)<abs(Lambda2)
+check=np.abs(mu1)>np.abs(mu2)
+Lambda1=mu1
+Lambda1[check]=mu2[check]
+Lambda2=mu2
+Lambda2[check]=mu1[check]
+Ix=v1x
+Ix[check]=v2x[check]
+Iy=v1y
+Iy[check]=v2y[check]
+
+# calculate taylor polynomial expension
+T = -np.divide((dx*Ix + dy*Iy),((ddx*Ix**2)+2*ddxy*Ix*Iy+(ddy*Iy**2)), where=((ddx*Ix**2)+2*ddxy*Ix*Iy+(ddy*Iy**2))!=0)
+
+# extract line (Px,Py)
+px = T*Ix
+py = T*Iy
+
+# generate candidate point (x,y)
+candidate_point = np.where((px >= -0.5) & (px <= 0.5) & (py >= -0.5) & (py <= 0.5))
+PX = candidate_point[0]
+PY = candidate_point[1]
